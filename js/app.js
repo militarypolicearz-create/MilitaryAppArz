3224cb5b6fb4a7d551e53fa2d828a79a348f8a88
militarypolicearz-create <militarypolice.arz@gmail.com>
2026-07-01 16:38:01 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -855,8 +855,8 @@ const FIXED_EMPLOYEE_STRUCTURE = [
     { id: 'officer_7', position: 'Офицер ВП', type: 'officer', username: 'Kristoph_Hidenberg' },
     { id: 'officer_6', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'cadet_2', position: 'Курсант ВП', type: 'cadet', username: 'Itzi_RealMe' },
-    { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
-    { id: 'cadet_3', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' }
+    { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'ExSean_Krulovна' },
+    { id: 'cadet_3', position: 'Курсант ВП', type: 'cadet', username: 'Orbit_White' }
 ];
 
 // === ВОПРОСЫ ДЛЯ ЭКЗАМЕНА ===
