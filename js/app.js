6e8da504287c3fc22138336d1ee13e97f7527d42
militarypolicearz-create <militarypolice.arz@gmail.com>
2026-07-14 11:35:11 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -773,7 +773,7 @@ const FIXED_EMPLOYEE_STRUCTURE = [
     { id: 'officer_7', position: 'Офицер ВП', type: 'officer', username: 'Kristoph_Hidenberg' },
     { id: 'officer_6', position: 'Офицер ВП', type: 'officer', username: 'Maks_Willov' },
     { id: 'officer_4', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
-    { id: 'cadet_2', position: 'Курсант ВП', type: 'cadet', username: 'Orbit_White' },
+    { id: 'cadet_2', position: 'Курсант ВП', type: 'cadet', username: 'Vakh_Hills' },
     { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
     { id: 'cadet_3', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' }
 ];
