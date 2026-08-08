244722727977c39a22b76784a2255607a9155c3d
militarypolicearz-create <militarypolice.arz@gmail.com>
2026-08-08 10:06:45 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -776,7 +776,7 @@ const FIXED_EMPLOYEE_STRUCTURE = [
     { id: 'officer_8', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'officer_4', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'officer_10', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
-    { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
+    { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'Kiril_Kot' },
     { id: 'cadet_3', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
     { id: 'cadet_2', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
     { id: 'cadet_4', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
